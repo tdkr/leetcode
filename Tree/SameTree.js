532126f4e4f5c5c976519f675ccedeb7cb692442
ronan <lo.n@qq.com>
2015-05-09 17:57:13 +0800

    Add SameTree Solution

---
+++ Tree/SameTree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+	if (p === null) return q === null;
+	if (q === null) return p === null;
+
+	if (p.val === q.val) {
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	} else {
+		return false;
+	}
+};
