22be615544dc5e5c628cf59791c73195b0727bbc
ronan <lo.n@qq.com>
2015-05-23 16:29:38 +0800

    LRUCache solution update

--- DataStructure/LRUCache.cs
+++ DataStructure/LRUCache.cs
@@ -1,33 +1,58 @@
-using System.Collection.Generic;
+using System.Collections.Generic;
 
-public class LRUCache {
-	int latestUsedKey;
+public class LRUCache
+{
+    int capacity = 0;
+    Dictionary<int, int> valueDic = new Dictionary<int, int>();
+    Dictionary<int, LinkedListNode<int>> nodeDic = new Dictionary<int, LinkedListNode<int>>();
 
-	int capacity = 0;
-	Dictionary<int, int> map = new Dictionary<int,int>();
+    LinkedList<int> usedKeyList = new LinkedList<int>();
 
-    public LRUCache(int capacity) {
+    public LRUCache(int capacity)
+    {
         this.capacity = capacity;
     }
 
-    public int Get(int key) {
-    	latestUsedKey = key;
+    public int Get(int key)
+    {
+        if (valueDic.ContainsKey(key))
+        {
+            LinkedListNode<int> node = nodeDic[key];
+            usedKeyList.Remove(node);
+            usedKeyList.AddFirst(node);
 
-        if(map.Contains(key) return map[key]);
+            return valueDic[key];
+        }
         return -1;
     }
 
-    public void Set(int key, int value) {     
-    	if(map.Count < capacity)
-    	{   
-        if(map.Contains(key) 
-        	map[key] = value;
-        else
-        	map.Add(key, value);
+    public void Set(int key, int value)
+    {
+        if (valueDic.ContainsKey(key))
+        {
+            valueDic[key] = value;
+            
+            LinkedListNode<int> node = nodeDic[key];
+            usedKeyList.Remove(node);
+            usedKeyList.AddFirst(node);
         }
-        else {
-        	
-        	
+        else
+        {
+            if (valueDic.Count >= capacity)
+            {
+                int removeKey = usedKeyList.Last.Value;
+                LinkedListNode<int> removeNode = nodeDic[removeKey];
+
+                valueDic.Remove(removeKey);
+                nodeDic.Remove(removeKey);
+                usedKeyList.Remove(removeNode);
+            }
+
+            valueDic.Add(key, value);
+
+            LinkedListNode<int> node = new LinkedListNode<int>(key);
+            usedKeyList.AddFirst(node);
+            nodeDic.Add(key, node);
         }
     }
 }
